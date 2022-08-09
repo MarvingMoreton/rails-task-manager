4d12b7bec259b004243c67ff2f218cafc82fbed1
Marving Moreton <moreton.marving@gmail.com>
2022-08-09 14:13:28 -0400

    solution without optional

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,13 +3,46 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     # Save the instance of Task in a instance variable named task
     @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # No need a view create.html.erb so redirect:
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
     # raise
-    # @task = Task.new(params[:])
+    @task.update(task_params)
+    # view not needed so redirect to the task individual page:
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    #
+    # no need for view destroy so redirect with 303:
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permite(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-
   # READ ALL
-  get "tasks", to: "tasks#index"
+  get 'tasks', to: 'tasks#index'
 
   # Create the GET request for the restaurant creation form
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
 
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
+
+  # READ ONE - with parameter so LAST
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  post 'tasks', to: 'tasks#create'
 end
